abd006f7e5db86324d31cd950178cf463ce7db1c
Isaiah Johnson <johnsonisaiah13@yahoo.com>
2023-02-09 11:30:32 -0600

    Checkout.js can now map through all items that are in the cart and display them all. No CSS at this time.

--- src/Components/Checkout.js
+++ src/Components/Checkout.js
@@ -1,9 +1,14 @@
 import React from 'react'
 import '../CSS Files/Checkout.css'
+import { useStateValue } from './StateProvider'
 
 import SubTotal from './SubTotal.js'
+import CheckoutProduct from './CheckoutProduct.js'
 
 function Checkout() {
+
+  const [{ cart }, dispatch] = useStateValue()
+
   return (
     <div className='checkout'>
         <div className='checkout_left'>
@@ -12,8 +17,11 @@ function Checkout() {
             <div>
                 <h2 className='checkout_title'>Your Shopping Cart</h2>
 
+                {cart.map(item => (
+                  <CheckoutProduct id={item.id} title={item.title} image={item.image} price={item.price} rating={item.rating} />
+                ))}
                 {/* CHECKOUT PRODUCT  */}
-                
+
                 {/* CHECKOUT PRODUCT  */}
 
                 {/* CHECKOUT PRODUCT  */}
