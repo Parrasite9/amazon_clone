c33e9ecf62281ccd1b42a5bb01758ccde3ddafe3
Isaiah Johnson <johnsonisaiah13@yahoo.com>
2023-02-09 13:59:45 -0600

    signin and registration function outlines started, not completed. Have not started Firebase

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,9 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import '../CSS Files/Login.css'
 
 
 function Login() {
+
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+    const signIn = (e) => {
+        // PREVENTS PAGE FROM REFRESHING
+        e.preventDefault()
+
+        // SOME FANCY FIREBASE LOGIN 
+
+
+    }
+
+    const register = (e) => {
+        // PREVENTS PAGE FROM REFRESHING 
+        e.preventDefault()
+
+        // SOME FANCY FIREBASE ACCOUNT CREATION
+    }
+
   return (
     <div className='login'>
         <Link to='/'>
@@ -14,12 +34,14 @@ function Login() {
             <h1>Sign-In</h1>
             <form>
                 <h5>Email</h5>
-                <input type='text' />
+                <input type='text' value={email} onChange={e =>
+                    setEmail(e.target.value)} />
 
                 <h5>Password</h5>
-                <input type='password' />
+                <input type='password' value={password} onChange={e =>
+                    setPassword(e.target.value)} />
 
-                <button className='login_button'>Sign-In</button>
+                <button className='login_button' type='submit' onClick={signIn}>Sign-In</button>
             </form>
 
             <p>
@@ -27,7 +49,7 @@ function Login() {
                 Please see our Privacy Notice, our Cookies Notice and our Interest-Based Ads Notice
             </p>
 
-            <button className='register_button'>Create Your Amazon Account</button>
+            <button className='register_button' onClick={register}>Create Your Amazon Account</button>
         </div>
     </div>
 
